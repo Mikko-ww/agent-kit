e7dcce72274fb9062739251177096fdd473ac3b6
Allen <wei125423412@163.com>
2026-03-27 17:09:44 +0800

    发布 opencode-env-switch v0.2.5

--- packages/opencode-env-switch/src/opencode_env_switch/__init__.py
+++ packages/opencode-env-switch/src/opencode_env_switch/__init__.py
@@ -2,7 +2,7 @@
 
 __all__ = ["API_VERSION", "CONFIG_VERSION", "PLUGIN_ID", "__version__"]
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 PLUGIN_ID = "opencode-env-switch"
 API_VERSION = 1
 CONFIG_VERSION = 1
