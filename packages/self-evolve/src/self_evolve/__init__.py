5f7d2d6bef3c297e1b436dcb568d067696a2e9d2
Allen <wei125423412@163.com>
2026-04-01 13:51:23 +0800

    发布 self-evolve v0.6.1

--- packages/self-evolve/src/self_evolve/__init__.py
+++ packages/self-evolve/src/self_evolve/__init__.py
@@ -2,7 +2,7 @@
 
 __all__ = ["API_VERSION", "CONFIG_VERSION", "PLUGIN_ID", "__version__"]
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 PLUGIN_ID = "self-evolve"
 API_VERSION = 1
 CONFIG_VERSION = 5
