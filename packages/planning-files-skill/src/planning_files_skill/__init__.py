90c784de9369e0717a8df7451c667ba037de8eae
Allen <wei125423412@163.com>
2026-04-30 16:15:02 +0800

    发布 planning-files-skill v0.1.1

--- packages/planning-files-skill/src/planning_files_skill/__init__.py
+++ packages/planning-files-skill/src/planning_files_skill/__init__.py
@@ -2,7 +2,7 @@
 
 __all__ = ["API_VERSION", "CONFIG_VERSION", "PLUGIN_ID", "SKILL_NAME", "__version__"]
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 PLUGIN_ID = "planning-files-skill"
 SKILL_NAME = "planning-files"
 API_VERSION = 1
